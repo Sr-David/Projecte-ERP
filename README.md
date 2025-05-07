d58fa91534d7871d2dae938129eabe3f0a571c0a
David_Calvo <david.calvo@mataro.epiaedu.cat>
2025-05-07 19:01:15 +0200

    DB dump correcto, instrucciones actualizadas

--- README.md
+++ README.md
@@ -16,13 +16,41 @@ Después de esto, es necesario ejecutar el comando powershell:
 
 En este paso ya se puede probar la conexión A la base de datos. Se crea una conexión con el SGDBDD y se importa el dump-ERP_CRM-xxxx.sql
 
-A continuación 
+# Como cargar la estructura de la base de datos
+
+Para cargar la estructura de la base de datos se utiliza el archivo dump-erp_crm.sql. 
+Primeramente se copia el archivo dentro del contenedor de la base de datos
+
+    docker cp .\dump-erp_crm.sql crm-erp-BD:/dump.sql
+
+A continuación entramos en la linea de comandos interna del contenedor
+
+    docker exec -it crm-erp-BD bash
+
+Ahora nos disponemos a ejecutar el dump en la base de datos
+
+    mysql -u root -p laravel < /dump.sql
+
+Solo hay que introducir la contraseña del docker-compose y esperar a que se cargue el proceso (suele ser inmediato)
+
+# Ultimos pasos
+
+
+A continuación se realizan los ultimos retoques para que los permisos permitan al contenedor funcionar como debe
 
     docker exec -it laravel_app bash
+
+Asignamos permisos
     
     chmod -R 775 storage bootstrap/cache
     
+Ahora la siguiente comanda
+
     chown -R www-data:www-data storage bootstrap/cache
-    
+
+Ya podemos salir
+
     exit
 
+
+Después de estos pasos ya se puede trabajar con el proyecto. 
---
+++ dump-erp_crm.sql
@@ -0,0 +1,141 @@
+-- MySQL dump 10.13  Distrib 9.1.0, for Linux (x86_64)
+--
+-- Host: localhost    Database: ERP_CRM
+-- ------------------------------------------------------
+-- Server version	9.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Clients`
+--
+
+DROP TABLE IF EXISTS `Clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Clients` (
+  `idClient` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) NOT NULL,
+  `LastName` varchar(100) NOT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Phone` varchar(50) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `ClientTypeID` int DEFAULT NULL,
+  PRIMARY KEY (`idClient`),
+  KEY `ClientTypeID` (`ClientTypeID`),
+  CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`ClientTypeID`) REFERENCES `ClientType` (`idClientType`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ClientType`
+--
+
+DROP TABLE IF EXISTS `ClientType`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ClientType` (
+  `idClientType` int NOT NULL AUTO_INCREMENT,
+  `ClientType` varchar(45) NOT NULL,
+  `Description` text,
+  PRIMARY KEY (`idClientType`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Where the client''s type is stored';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ProductsServices`
+--
+
+DROP TABLE IF EXISTS `ProductsServices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ProductsServices` (
+  `idProductService` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) NOT NULL,
+  `Description` text,
+  `Price` decimal(10,2) NOT NULL,
+  `Stock` int NOT NULL,
+  `EntryDate` datetime NOT NULL,
+  PRIMARY KEY (`idProductService`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `SalesDetails`
+--
+
+DROP TABLE IF EXISTS `SalesDetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SalesDetails` (
+  `idSaleDetail` int NOT NULL AUTO_INCREMENT,
+  `ProposalID` int DEFAULT NULL,
+  `ProductServiceID` int DEFAULT NULL,
+  `QuantitySold` int NOT NULL,
+  `UnitPrice` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`idSaleDetail`),
+  KEY `ProposalID` (`ProposalID`),
+  KEY `ProductServiceID` (`ProductServiceID`),
+  CONSTRAINT `salesdetails_ibfk_1` FOREIGN KEY (`ProposalID`) REFERENCES `SalesProposals` (`idSalesProposals`),
+  CONSTRAINT `salesdetails_ibfk_2` FOREIGN KEY (`ProductServiceID`) REFERENCES `ProductsServices` (`idProductService`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `SalesProposals`
+--
+
+DROP TABLE IF EXISTS `SalesProposals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SalesProposals` (
+  `idSalesProposals` int NOT NULL,
+  `ClientID` int DEFAULT NULL,
+  `CreatedAt` datetime DEFAULT NULL,
+  `State` varchar(45) DEFAULT NULL,
+  `Details` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idSalesProposals`),
+  KEY `fk_salesproposals_client` (`ClientID`),
+  CONSTRAINT `fk_salesproposals_client` FOREIGN KEY (`ClientID`) REFERENCES `Clients` (`idClient`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `UserAdministration`
+--
+
+DROP TABLE IF EXISTS `UserAdministration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `UserAdministration` (
+  `idUser` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(100) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Permissions` text,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'ERP_CRM'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-07 16:09:49
