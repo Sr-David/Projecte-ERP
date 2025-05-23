7df6834e76d9e27cf54c1804372292b4841e6e1f
David_Calvo <david.calvo@mataro.epiaedu.cat>
2025-05-23 18:50:35 +0200

    Created add en tabla salesDetails del Dump

--- dump-erp_crm.sql
+++ dump-erp_crm.sql
@@ -94,14 +94,14 @@ CREATE TABLE `SalesDetails` (
   `QuantitySold` int NOT NULL,
   `UnitPrice` decimal(10,2) NOT NULL,
   `idEmpresa` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`idSaleDetail`),
   KEY `ProposalID` (`ProposalID`),
   KEY `ProductServiceID` (`ProductServiceID`),
   KEY `idEmpresa` (`idEmpresa`),
-  CONSTRAINT `salesdetails_ibfk_1` FOREIGN KEY (`ProposalID`) REFERENCES `SalesProposals` (`idSalesProposals`),
   CONSTRAINT `salesdetails_ibfk_2` FOREIGN KEY (`ProductServiceID`) REFERENCES `ProductsServices` (`idProductService`),
   CONSTRAINT `salesdetails_ibfk_3` FOREIGN KEY (`idEmpresa`) REFERENCES `UserAdministration` (`idEmpresa`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
